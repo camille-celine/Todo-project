f09683b4a41e7eca1fbc9c73b34b1b7d8f99b54a
Camille Rey <camille.celine.rey@gmail.com>
2022-02-11 10:37:04 +0100

    Form and List with Todos CRUD

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,34 @@
 import React from "react";
+import Todo from "./Todo";
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 
-function TodoList() {
+function TodoList(props) {
   return (
-    <div>
-      <p>TodoList</p>
-    </div>
+    <Paper>
+      <List>
+        {props.todos.map(todo => (
+          <>
+            <ListItem>
+              <ListItemText>
+                <Todo 
+                  task={todo.task} 
+                  id={todo.id}
+                  key={todo.id} 
+                  completed={todo.completed}
+                  removeTodo={props.removeTodo}
+                  editTodo={props.editTodo}
+                />
+              </ListItemText>
+            </ListItem>
+            <Divider/>
+          </>
+        ))}
+      </List>
+    </Paper>
   );
 }
 
